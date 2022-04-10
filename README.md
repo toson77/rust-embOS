f7b3bf7a27b4aa32ab5ed3129635f8c6e585e026
nori <norifxsc@gmail.com>
2022-04-10 12:38:04 +0900

    add ld

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # rust_embOS
-reference
+Hobby embedded OS for stm32f401
+Reference
 https://garasubo.github.io/embedded-book/
 
--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,14 @@ use core::panic::PanicInfo;
 fn panic(_panic: &PanicInfo<'_>) -> ! {
     loop {}
 }
+
+// The reset vector, a pointer into the reset handler
+#[link_section = ".vector_table.reset_vector"]
+#[no_mangle]
+pub static RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
+
+#[no_mangle]
+pub unsafe extern "C" fn Reset() -> ! {
+    let _x = 42;
+    loop {}
+}
