f90741c3253e7663ed41bec188aee6ef245e349f
nori <norifxsc@gmail.com>
2022-07-17 17:44:10 +0900

    add roundrobin

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,12 @@
 use core::panic::PanicInfo;
 use core::ptr;
 use cortex_m_semihosting::hprintln;
+mod linked_list;
+use linked_list::ListItem;
 mod process;
+use process::Process;
+mod scheduler;
+use scheduler::Scheduler;
 mod systick;
 use process::ContextFrame;
 
@@ -40,7 +45,23 @@ pub unsafe extern "C" fn Reset() -> ! {
     systick::init();
 
     #[link_section = ".app_stack"]
-    static mut APP_STACK: [u8; 1024] = [0; 1024];
+    static mut APP_STACK: [u8; 2048] = [0; 2048];
+    #[link_section = ".app_stack"]
+    static mut APP_STACK2: [u8; 2048] = [0; 2048];
+    #[link_section = ".app_stack"]
+    static mut APP_STACK3: [u8; 2048] = [0; 2048];
+    let mut process1 = Process::new(&mut APP_STACK, app_main);
+    let mut item1 = ListItem::new(process1);
+    let process2 = Process::new(&mut APP_STACK2, app_main2);
+    let mut item2 = ListItem::new(process2);
+    let process3 = Process::new(&mut APP_STACK3, app_main3);
+    let mut item3 = ListItem::new(process3);
+    let mut sched = Scheduler::new();
+    sched.push(&mut item1);
+    sched.push(&mut item2);
+    sched.push(&mut item3);
+    sched.exec();
+    // hprintln!("Kernel").unwrap();
 
     let ptr = (&APP_STACK[0] as *const u8 as usize) + 1024 - 0x20;
     let context_frame: &mut ContextFrame = &mut *(ptr as *mut ContextFrame);
@@ -133,9 +154,28 @@ pub unsafe extern "C" fn SVCall() {
 }
 
 extern "C" fn app_main() -> ! {
-    hprintln!("App").unwrap();
-    unsafe {
-        asm!("svc 0"::::"volatile");
+    let mut i = 0;
+    loop {
+        hprintln!("App:{}", i).unwrap();
+        unsafe {
+            asm!("svc 0"::::"volatile");
+            i += 1;
+        }
     }
-    loop {}
+}
+extern "C" fn app_main2() -> ! {
+    loop {
+        hprintln!("App2").unwrap();
+        unsafe {
+            asm!("svc 0"::::"volatile");
+        }
+    }
+}
+extern "C" fn app_main3() -> ! {
+    loop {
+        hprintln!("App3").unwrap();
+        unsafe {
+            asm!("svc 0"::::"volatile");
+        }
+    }
 }
--- src/process.rs
+++ src/process.rs
@@ -1,3 +1,4 @@
+use core::marker::PhantomData;
 #[repr(C)]
 pub struct ContextFrame {
     pub r0: u32,
@@ -8,4 +9,48 @@ pub struct ContextFrame {
     pub lr: u32,
     pub return_addr: u32,
     pub xpsr: u32,
+}
+
+pub struct Process<'a> {
+    sp: *mut u8,
+    regs: [u32; 8],
+    marker: PhantomData<&'a u8>,
+}
+
+impl<'a> Process<'a> {
+    pub fn new(stack: &'a mut [u8], app_main: extern "C" fn() -> !) -> Self {
+        let sp = (&stack[0] as *const u8 as usize) + stack.len() - 0x20;
+        let context_freame: &mut ContextFrame = unsafe { &mut *(sp as *mut ContextFrame) };
+        context_freame.r0 = 0;
+        context_freame.r1 = 0;
+        context_freame.r3 = 0;
+        context_freame.r12 = 0;
+        context_freame.lr = 0;
+        context_freame.return_addr = app_main as u32;
+        context_freame.xpsr = 0x0100_0000;
+
+        Process {
+            sp: sp as *mut u8,
+            regs: [0; 8],
+            marker: PhantomData,
+        }
+    }
+
+    pub fn exec(&mut self) {
+        unsafe {
+            asm!(
+                "
+                msr psp, r0
+                ldmia r1, {r4-r11}
+                svc 0
+                stmia r1, {r4-r11}
+                mrs r0, psp
+                "
+                :"={r0}"(self.sp)
+                :"{r0}"(self.sp), "{r1}"(&self.regs)
+                :"r4","r5","r6","r8","r9","r10","r11"
+                :"volatile"
+            );
+        }
+    }
 }
